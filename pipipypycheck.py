e50d2f9448dac0c15f3e50ddbd045ee79b0435ac
KuRAmic <darfagel@gmail.com>
2023-04-26 17:49:24 +0500

    987

--- pipipypycheck.py
+++ pipipypycheck.py
@@ -1,113 +1,79 @@
-from tkinter import * 
- 
-shkaf = 0
+from tkinter import *
+from tkinter import ttk
 
-flag = False 
-a = 1 
- 
+shkaf = [[]]
+flag = False
+a = 0
+
+def fun(a):
+    polka = Tk()
+    polka.title('Полка' + str(a + 1))
+    polka.geometry('600x300')
+    for i in range(len(shkaf[a])):
+        print(shkaf[a][i])
+        btn = Button(polka, text=shkaf[a][i], relief=RIDGE)
+        btn.grid(column=10, row=10)
+        btn.place(relx= 0.5, rely=0.1 * i)
+
 def btn_fun(a):
-    global flag 
+    global flag
     global shkaf
+    if a == 1:
+        print('1')
+        shkaf = [[1, 2, 3], [4, 5], [6]]
+    if a == 2:
+        print('2')
+        shkaf = [[11, 22], [444, 4, 44]]
+    if a == 3:
+        print('3')
+        shkaf = [[3, 5]]
     flag=True
-    window.destroy() 
+    window.destroy()
 
-def btn_fin1():
-    global flag
-    flag=True
-    sklad.destroy()
+def new():
+    global shkaf
+    shkaf.append([])
+    for i in range(len(shkaf)):
+        btn_name = 'Полка' + str(i + 1)
+        btn = Button(sklad, text=btn_name, relief=RIDGE, command=lambda i=i: fun(i))
+        btn.grid(column=10, row=10)
+        btn.place(relx= 0.1 * (i % 10), rely=0.09 * (i // 10))
 
-def btn_fin2():
-    global flag
-    flag=True
-    prolet.destroy()
+if not flag:
 
-def btn_fin3():
-    global flag
-    flag=True
-    polka.destroy()
+    window = Tk()
+    window.title('СКЛАДЫ')
+    window.geometry('800x600')
 
+    btn1 = Button(window, text='Склад 1', relief=RIDGE, command=lambda: btn_fun(1))
+    btn1.grid(column=0, row=0)
+    btn1.place(relx=0.25, rely=0.5)
 
+    btn2 = Button(window, text='Склад 2', relief=RIDGE, command=lambda: btn_fun(2))
+    btn2.grid(column=0, row=0)
+    btn2.place(relx=0.5, rely=0.5)
 
+    btn3 = Button(window, text='Склад 3', relief=RIDGE, command=lambda: btn_fun(3))
+    btn3.grid(column=0, row=0)
+    btn3.place(relx=0.75, rely=0.5)
 
+    window.mainloop()
 
-    
-if not flag: 
- 
-    window = Tk() 
-    window.title('СКЛАДЫ') 
-    window.geometry('1000x1000') 
-    
-    btn1 = Button(window, text='Склад 1', relief=RIDGE, command=lambda:btn_fun(1)) 
-    btn1.grid(column=0, row=0) 
-    btn1.place(relx=0.25, rely=0.5) 
-    
-    btn2 = Button(window, text='Склад 2', relief=RIDGE, command=lambda:btn_fun(2)) 
-    btn2.grid(column=0, row=0) 
-    btn2.place(relx=0.5, rely=0.5) 
-    
-    btn3 = Button(window, text='Склад 3', relief=RIDGE, command=lambda:btn_fun(3)) 
-    btn3.grid(column=0, row=0) 
-    btn3.place(relx=0.75, rely=0.5) 
-    
-    window.mainloop() 
+if flag:
 
+    sklad = Tk()
+    sklad.title('Склад' + str(a))
+    sklad.geometry('800x600')
+    sklad.size()
+    for i in range(len(shkaf)):
+        btn_name = 'Полка' + str(i + 1)
+        btn = Button(sklad, text=btn_name, relief=RIDGE, command=lambda i=i: fun(i))
+        btn.grid(column=10, row=10)
+        btn.place(relx= 0.1 * (i % 10), rely=0.09 * (i // 10))
 
-if flag: 
- 
-    sklad = Tk() 
-    sklad.title('Склад ' + str(a)) 
-    sklad.geometry('1000x1000') 
-    flag=False
-    for i in range(4): 
-        btn_name = 'Пролёт ' + str(i + 1) 
-        btn = Button(sklad, text = btn_name,relief=RIDGE, command=btn_fin1) 
-        btn.grid(column=0, row=0) 
-        btn.place(relx= 0.2 * i, rely=0.2) 
-    
+        new = Button(sklad, text='Добавить кнопку', relief=RIDGE, command=new)
+        new.grid(column=10, row=10)
+        new.place(relx=0.85, rely=0.95)
+
     sklad.mainloop()
-
-
-
-if flag: 
- 
-    prolet = Tk() 
-    prolet.title('Пролёт ' + str(a)) 
-    prolet.geometry('1000x1000') 
-    flag=False
-    for i in range(4): 
-        btn_name = 'Полка ' + str(i + 1) 
-        btn = Button(prolet, text = btn_name,relief=RIDGE, command=btn_fin2) 
-        btn.grid(column=0, row=0) 
-        btn.place(relx= 0.2 * i, rely=0.2) 
     
-    prolet.mainloop()
-
-if flag: 
- 
-    polka = Tk() 
-    polka.title('Полка ' + str(a)) 
-    polka.geometry('1000x1000') 
-    flag=False
-    for i in range(4): 
-        btn_name = 'Ячейка ' + str(i + 1) 
-        btn = Button(polka, text = btn_name,relief=RIDGE, command=btn_fin3) 
-        btn.grid(column=0, row=0) 
-        btn.place(relx= 0.2 * i, rely=0.2) 
-    
-    polka.mainloop()
-
-
-if flag: 
- 
-    yacheika = Tk() 
-    yacheika.title('Ячейка ' + str(a)) 
-    yacheika.geometry('1000x1000') 
-    flag=False
-
-
-
-
-
-
-
-    yacheika.mainloop()
